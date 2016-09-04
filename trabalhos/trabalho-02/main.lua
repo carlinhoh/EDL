dd0f5e4b8f9c1e5ebaa53ba149cbe3c8a70d1a9a
carlinhoh <carloslucianosa@gmail.com>
2016-09-03 22:54:40 -0300

    Update main.lua

--- trabalhos/trabalho-02/main.lua
+++ trabalhos/trabalho-02/main.lua
@@ -1,15 +1,13 @@
--- Load some default values for our rectangle.
+-- teste
 function love.load()
     x, y, w, h = 20, 20, 60, 20
 end
  
--- Increase the size of the rectangle every frame.
 function love.update(dt)
     w = w + 1
     h = h + 1
 end
- 
--- Draw a coloured rectangle.
+
 function love.draw()
     love.graphics.setColor(0, 100, 100)
     love.graphics.rectangle("fill", x, y, w, h)
